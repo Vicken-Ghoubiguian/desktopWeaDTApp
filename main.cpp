c8116405db76f473ee2867d8a2bfd2287b166f87
Eric Ghoubiguian <ericghoubiguian@live.fr>
2021-11-03 12:00:59 +0100

    Some preparations

--- main.cpp
+++ main.cpp
@@ -1,23 +1,54 @@
+//
 #include "maindesktopweadtwindow.h"
 
+//
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
 
+//
 int main(int argc, char *argv[])
 {
+    //
     QApplication a(argc, argv);
 
+    /*
+
+    */
+
+    //
     QTranslator translator;
+
+    //
     const QStringList uiLanguages = QLocale::system().uiLanguages();
+
+    //
     for (const QString &locale : uiLanguages) {
+
+        //
         const QString baseName = "desktopWeaDTApp_" + QLocale(locale).name();
+
+        //
         if (translator.load(":/i18n/" + baseName)) {
+
+            //
             a.installTranslator(&translator);
+
+            //
             break;
         }
     }
+
+    /*
+
+    */
+
+    //
     MainDesktopWeaDTWindow w;
+
+    //
     w.show();
+
+    //
     return a.exec();
 }
