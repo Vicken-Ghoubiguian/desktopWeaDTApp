bef99350af726679f24532683ec0e9c459ea2db3
Eric Ghoubiguian <ericghoubiguian@live.fr>
2021-12-04 19:29:13 +0100

    UII 3

--- countryflag.cpp
+++ countryflag.cpp
@@ -5,3 +5,19 @@ countryFlag::countryFlag(QObject *parent) : QObject(parent)
 {
 
 }
+
+//
+QString countryFlag::getFlagURL()
+{
+    return this->flagURL;
+}
+
+QString countryFlag::getCountryCode()
+{
+    return this->countryCode;
+}
+
+QString countryFlag::getCountryName()
+{
+    return this->countryName;
+}
