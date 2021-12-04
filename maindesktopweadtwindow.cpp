58b83f84757c7ef3f1e53cd4832f5d9c842bf95a
Eric Ghoubiguian <ericghoubiguian@live.fr>
2021-12-04 10:16:19 +0100

    GHYYU

--- maindesktopweadtwindow.cpp
+++ maindesktopweadtwindow.cpp
@@ -10,7 +10,26 @@ MainDesktopWeaDTWindow::MainDesktopWeaDTWindow(QWidget *parent)
     , ui(new Ui::MainDesktopWeaDTWindow)
 {
     //
-    this->allTimezonesQList = QTimeZone::availableTimeZoneIds();
+    QList<QByteArray> listOfAllIANATimezones = QTimeZone::availableTimeZoneIds();
+
+    //
+    for(int i = 0; i < listOfAllIANATimezones.count(); i++) {
+
+        //
+        QString currentTimeZoneAsString = QString(listOfAllIANATimezones[i]);
+
+        //
+        QStringList currentTimeZoneAsStringList = currentTimeZoneAsString.split("/");
+
+        //
+        if(currentTimeZoneAsStringList.contains("Africa") || currentTimeZoneAsStringList.contains("America") || currentTimeZoneAsStringList.contains("Asia") || currentTimeZoneAsStringList.contains("Atlantic") || currentTimeZoneAsStringList.contains("Australia") || currentTimeZoneAsStringList.contains("Europe") || currentTimeZoneAsStringList.contains("Indian") || currentTimeZoneAsStringList.contains("Pacific")) {
+
+            this->allTimezonesQList = QTimeZone::availableTimeZoneIds();
+        }
+    }
+
+    //
+    //this->allTimezonesQList = QTimeZone::availableTimeZoneIds();
 
     //
     ui->setupUi(this);
