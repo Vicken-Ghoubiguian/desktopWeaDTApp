0eede5667ec9af82f314453792c808d9a36f7147
Eric Ghoubiguian <ericghoubiguian@live.fr>
2022-11-25 12:07:59 +0100

    For UV constructor

--- ultraviolet.cpp
+++ ultraviolet.cpp
@@ -4,7 +4,39 @@ UltraViolet::UltraViolet(int value)
 {
 
     //
+    if(value <= 2) {
 
+        //
+        this->risk = Low;
+    }
+
+    //
+    else if (3 <= value && value <= 5) {
+
+        //
+        this->risk = Moderate;
+    }
+
+    //
+    else if (6 <= value && value <= 7) {
+
+        //
+        this->risk = High;
+    }
+
+    //
+    else if(8 <= value && value <= 10) {
+
+        //
+        this->risk = Very_High;
+    }
+
+    //
+    else {
+
+        //
+        this->risk = Extreme;
+    }
 
     //
     this->index = value;
