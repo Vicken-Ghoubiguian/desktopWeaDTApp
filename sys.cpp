cd2d34481b59be3f5b075768881dfdda26663607
Eric Ghoubiguian <ericghoubiguian@live.fr>
2022-12-12 10:31:52 +0100

    Timie

--- sys.cpp
+++ sys.cpp
@@ -6,6 +6,40 @@ Sys::Sys()
 }
 
 //
+QString Sys::getCountry()
+{
+    //
+    return this->country;
+}
+
+//
+int Sys::getId()
+{
+    //
+    return this->id;
+}
+
+//
+int Sys::getSunrise()
+{
+    //
+    return this->sunrise;
+}
+
+//
+int Sys::getSunset()
+{
+    //
+    return this->sunset;
+}
+
+int Sys::getType()
+{
+    //
+    return this->type;
+}
+
+//
 QString Sys::toString()
 {
     return "";
