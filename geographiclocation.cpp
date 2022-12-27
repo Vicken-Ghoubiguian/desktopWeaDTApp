4875a0e7c471a294c4b6ecdb2157ea6e80fcd08b
Eric Ghoubiguian <ericghoubiguian@live.fr>
2022-12-27 12:40:10 +0100

    Klop

--- geographiclocation.cpp
+++ geographiclocation.cpp
@@ -6,3 +6,9 @@ GeographicLocation::GeographicLocation(QString countryCode, QString cityName)
     this->countryCode = countryCode;
     this->cityName = cityName;
 }
+
+//
+QString GeographicLocation::toString()
+{
+    return "";
+}
